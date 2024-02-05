23d70ad95e69493a9aa481d2d5793f6e09cc4aa8
Dev2vinod <vinodsinghfun@gmail.com>
2024-02-05 07:44:28 +0530

    sign up form front and back end

--- server.mjs
+++ server.mjs
@@ -1,34 +1,126 @@
 import express from 'express'
  import cors from 'cors'
-const app = express();
+  const app = express();
  app.use(cors());
-app.use(express.json()); // data in encrepted form
+  app.use(express.json()); // data in encrepted form
  const port = process.env.PORT || 3000; // hame nhi pata heroku par kon se port hai ya nhi abhi 
 
 
- let users  =[];
+ let users  =[];// later replace with monogodb 
+
+ // generate idd
+
+ function randomNumber(){
+  return Math.floor(Math.random()*1000000000);
+ }
 
 
-// app.get('/home', (req, res) => {
-//   res.send('Hello  i am home page  i am using the postman      i have not written the frontend !  my localhost adderess is   http://localhost:3000/')
-// })
-// app.get('/users', (req, res) => {
-//   res.send('Hello  i am home page  i am using the postman      i have not written the frontend !')
-// })
-app.put('/users', (req, res) => {
-  res.send('your user is modified on 3 feb 2024')
-})
-app.get('/user', (req, res) => {
+
+app.get('/user/:userId', (req, res) => {
+
+  let userId =req.params.userId;
+  let isFound =false;
+
+   for(let i =0;i<users.length;i++){
+
+    if(users[i].id ==userId){
+      res.send(users[i]);
+      isFound =true;
+      break;
+    }
+
+    if(!isFound){
+      res.send("user is not found")
+    }
+
+
+   }
+ 
+
+
   res.send(users);
+  console.log("single user ")
+})
+app.get('/users', (req, res) => { // for a multiple user
+  res.send(users);
+  console.log("for a mulitiple user ")
+})
+
+ app.put('/user/:userId', (req, res) => {
+ 
+  let userId =req.params.userId;
+  let userIndex =-1 ;
+ 
+  for(let i =0;i<users.length;i++){
+
+    if(users[i].id ==userId){
+      res.send(users[i]);
+      userIndex =i;
+      
+      break;
+    }
+    if(userIndex ===-1){
+      res.send("user not found");
+
+    }else{
+      if(req.body.fullname){
+        users[userIndex].fullname =req.body.fullname
+      };
+      if(req.body.username){
+        users[userIndex].username =req.body.username
+      };
+      if(req.body.password){
+        users[userIndex].password =req.body.password
+      };
+
+      res.send(users[userIndex]);
+    }
+
+
+
+  res.send('your user is modified on 4  feb 2024*******  a single user')
+}})
+ app.put('/users', (req, res) => {
+  res.send('your user is modified on 4 feb 2024 **** for multiple user')
 })
 app.post('/user', (req, res) => {
 
-  users.push(req.body);
-  console.log(req.body);
-  res.send('your user is created on 3 feb 2024')
+  let newUser ={
+    id:randomNumber(),
+    fullname:req.body.fullname,
+    username:req.body.username,
+    password:req.body.password,
+    message:"i am message from  server site  i have post method in line number 93"
+  }
+  users.push(newUser);
+  console.log(newUser);
+  res.send('your user is created on 4 feb 2024')
 })
+app.delete('/user/:userId', (req, res) => {
+
+  let userId =req.params.userId;
+  let userIndex =-1 ;
+ 
+  for(let i =0;i<users.length;i++){
+
+    if(users[i].id ==userId){
+      res.send(users[i]);
+      userIndex =i;
+      
+      break;
+    }
+    if(userIndex ===-1){
+      res.send("user is not found");
+
+    }else{
+      delete users[userIndex];
+    }
+
+
+  res.send('your user is deleted on 3 feb 2024 **** single user')
+}})
 app.delete('/users', (req, res) => {
-  res.send('your user is deleted on 3 feb 2024')
+  res.send('your user is deleted on 3 feb 2024 *** multiple userr')
 })
 
 app.get('/profile', (req, res) => {
